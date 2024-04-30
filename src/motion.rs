f8538eced07e80ea53c7cf83112a898706c063cd
Papsanly <papsanlysenko@gmail.com>
2024-05-01 02:54:49 +0300

    rework motion physics (change acceleration to mass)

--- src/motion.rs
+++ src/motion.rs
@@ -2,13 +2,13 @@ use bevy::prelude::*;
 use std::f32::consts::{FRAC_PI_2, SQRT_2};
 
 const FRICTION: f32 = 0.5;
-const ACCELERATION: f32 = 2.0;
+const MASS: f32 = 0.5;
 
 #[derive(Component, Default, Debug, Deref, DerefMut)]
 pub struct Velocity(pub Vec3);
 
 #[derive(Component, Default, Debug, Deref, DerefMut)]
-pub struct Acceleration(pub Vec3);
+pub struct Force(pub Vec3);
 
 #[derive(Component)]
 pub struct KeyboardMotion;
@@ -16,7 +16,7 @@ pub struct KeyboardMotion;
 #[derive(Bundle, Default)]
 pub struct MotionBundle {
     velocity: Velocity,
-    acceleration: Acceleration,
+    force: Force,
 }
 
 pub struct MotionPlugin;
@@ -27,60 +27,57 @@ impl Plugin for MotionPlugin {
     }
 }
 
-fn update_motion(
-    mut query: Query<(&mut Transform, &mut Velocity, &Acceleration)>,
-    time: Res<Time>,
-) {
-    for (mut transform, mut velocity, acceleration) in &mut query {
+fn update_motion(mut query: Query<(&mut Transform, &mut Velocity, &Force)>, time: Res<Time>) {
+    for (mut transform, mut velocity, force) in &mut query {
         let dt = time.delta_seconds();
-        **velocity = **velocity + (**acceleration - FRICTION * **velocity) * dt;
-        transform.translation += **velocity * dt;
+        let v = **velocity;
+        **velocity += (**force - FRICTION * v) / MASS * dt;
+        transform.translation += v * dt;
     }
 }
 
 fn keyboard_motion(
-    mut acceleration_query: Query<&mut Acceleration, With<KeyboardMotion>>,
+    mut force_query: Query<&mut Force, With<KeyboardMotion>>,
     camera_transform_query: Query<&Transform, With<Camera3d>>,
     input: Res<ButtonInput<KeyCode>>,
 ) {
     use KeyCode::{KeyA, KeyD, KeyS, KeyW};
 
-    let mut acceleration = acceleration_query.single_mut();
+    let mut force = force_query.single_mut();
     let &Transform {
         rotation: camera_rotation,
         ..
     } = camera_transform_query.single();
 
     let camera_direction = camera_rotation * Vec3::NEG_Z;
-    let camera_direction = Vec3::new(camera_direction.x, 0.0, camera_direction.z).normalize();
-    let forward = camera_direction * ACCELERATION;
-    let left = Quat::from_rotation_y(FRAC_PI_2) * forward * ACCELERATION;
+    let forward = Vec3::new(camera_direction.x, 0.0, camera_direction.z).normalize();
+    let right = Quat::from_rotation_y(-FRAC_PI_2) * forward;
 
     if input.all_pressed([KeyW, KeyD]) {
-        **acceleration = (forward - left) / SQRT_2;
+        **force = (forward + right) / SQRT_2;
     } else if input.all_pressed([KeyW, KeyA]) {
-        **acceleration = (forward + left) / SQRT_2;
+        **force = (forward - right) / SQRT_2;
     } else if input.all_pressed([KeyS, KeyD]) {
-        **acceleration = (-forward - left) / SQRT_2;
+        **force = (-forward + right) / SQRT_2;
     } else if input.all_pressed([KeyS, KeyA]) {
-        **acceleration = (-forward + left) / SQRT_2;
+        **force = (-forward - right) / SQRT_2;
     } else if input.pressed(KeyW) {
-        **acceleration = forward;
+        **force = forward;
     } else if input.pressed(KeyS) {
-        **acceleration = -forward;
+        **force = -forward;
     } else if input.pressed(KeyD) {
-        **acceleration = -left;
+        **force = right;
     } else if input.pressed(KeyA) {
-        **acceleration = left;
+        **force = -right;
     }
 
     if input.just_released(KeyW) {
-        **acceleration -= forward;
+        **force -= forward;
     } else if input.just_released(KeyS) {
-        **acceleration += forward;
+        **force += forward;
     } else if input.just_released(KeyD) {
-        **acceleration += left;
+        **force -= right;
     } else if input.just_released(KeyA) {
-        **acceleration -= left;
+        **force += right;
     }
 }
